ebe3f9b6b1496ba3af6e825f27f4522b704c3a87
Philipp Kapfer <philipp.kapfer@gmx.at>
2016-06-28 15:59:04 +0200

    Added silent mode

--- src/at/jku/fim/phonykeyboard/latin/utils/Log.java
+++ src/at/jku/fim/phonykeyboard/latin/utils/Log.java
@@ -2,9 +2,10 @@ package at.jku.fim.phonykeyboard.latin.utils;
 
 public class Log {
     private static boolean dataOnly = false;
+    private static boolean silent = false;
 
     public static void e(String tag, String message) {
-        if (!dataOnly) {
+        if (!silent && !dataOnly) {
             System.err.print(tag);
             System.err.print("\t[E] ");
             System.err.println(message);
@@ -12,7 +13,7 @@ public class Log {
     }
 
     public static void i(String tag, String message) {
-        if (!dataOnly) {
+        if (!silent && !dataOnly) {
             System.out.print(tag);
             System.out.print("\t[I] ");
             System.out.println(message);
@@ -23,8 +24,12 @@ public class Log {
         Log.dataOnly = dataOnly;
     }
 
+    public static void setSilent(boolean silent) {
+        Log.silent = silent;
+    }
+
     public static void data(String data) {
-        if (dataOnly) {
+        if (!silent && dataOnly) {
             System.out.println(data);
         }
     }
